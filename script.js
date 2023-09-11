91f9027bb7243db62cdab9a1802cf7d08d2d128f
klopp <rafedly17@gmail.com>
2023-09-11 16:25:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        body.classList.add('dark-mode');
+        body.classList.remove('light-mode');
+    } else {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+    }
+});
+
+
